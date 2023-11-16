1a1252577f1c7937b0412b035df52adc28f44b6d
Manuchexra <135632769+Manuchexra@users.noreply.github.com>
2023-11-16 19:43:00 +0500

    Update person.py

--- person.py
+++ person.py
@@ -1,1 +1,7 @@
 #Create a "Person" class
+class Person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+aboutMe=Person("Manuchexra",16)
+print(f'My name is {aboutMe.name} and i am {aboutMe.age} years old')
