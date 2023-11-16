5046e871822d9c782b7117e90ca34a28b95ca78a
Manuchexra <135632769+Manuchexra@users.noreply.github.com>
2023-11-16 19:28:16 +0500

    Update create_one_attribute.py

--- create_one_attribute.py
+++ create_one_attribute.py
@@ -1,2 +1,9 @@
 #Create a "Person" class
 #Create an attribute "name" in the "Person" class
+#Create a "Person" class
+#Create an attribute "name" in the "Person" class
+class Person:
+    def __init__(self,name):
+        self.name=name
+# p=Person('Manuchexra')
+# print(p.name)
